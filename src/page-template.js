d655a97562ea1a0e58eefc73319b72f2fa2117e5
Clay Abel <clayton.abel.95@gmail.com>
2022-01-28 14:58:11 -0600

    created the employee class and then extended that class to create the engineer, intern, and manager classes.

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
 //function that creates the html with placeholder data that will be passed through using index.js
 const Manager = require("../lib/Manager");
 const Intern = require("../lib/Intern");
-const Engineer = require("../lib/Engineer")
+const Engineer = require("../lib/Engineer");
