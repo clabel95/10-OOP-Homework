f70d0e57c9a0d3d09df4341e216733fd3bf52866
Clay Abel <clayton.abel.95@gmail.com>
2022-01-30 17:41:22 -0600

    added test for Employee

---
+++ tests/Employee.test.js
@@ -0,0 +1,29 @@
+
+const Employee = require("./lib/Employee");
+
+describe('Employee', () => {
+    describe('New', () => {
+        it("should create a new instance of employee with the given values", () =>{
+            const e = new Employee("john", 1, "john@john.com");
+            expect(e.name).toEqual("john");
+            expect(e.id).toEqual(1);
+            expect(e.email).toEqual("john@john.com")
+        });
+        it("should be able to retrieve the values for name using getName()", () =>{
+            const e = new Employee("john", 1, "john@john.com");
+            expect(e.getName()).toEqual("john");
+        });
+        it("should be able to retrieve the values for Id using getId()", () =>{
+            const e = new Employee("john", 1, "john@john.com");
+            expect(e.getId()).toEqual(1);
+        });
+        it("should be able to retrieve the values for Email using getEmail()", () =>{
+            const e = new Employee("john", 1, "john@john.com");
+            expect(e.getEmail()).toEqual("john@john.com");
+        });
+        it("should be able to retrieve the values for Role using getRole()", () =>{
+            const e = new Employee("john", 1, "john@john.com");
+            expect(e.getRole()).toEqual("Employee");
+        });
+    });
+});
